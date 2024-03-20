34cd83d1e6c5464e7dffe3d4b1849ca2cb305f0f
Tsirinirina <parajaonarison@gmail.com>
2024-03-20 18:48:31 +0100

    Ajout du nouvelle end-point Salut

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("salut")
+  getSalut(): string {
+    return this.appService.getSalut()
+  }
 }
